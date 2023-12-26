656710af8b48a897ee8209ff6cdfdab1ac61ff81
Guillottex <guillo.aiesec@gmail.com>
2023-12-26 08:26:42 -0300

    Segunda Pre-entrega

--- main.js
+++ main.js
@@ -1,38 +1,51 @@
 let cliente = prompt("Buenos días. ¿Cuál es tu nombre?");
 
-function bienvenido() {
-    let acumulado = {}; // Objeto para acumular el pedido
+// Array para almacenar los productos
+const productos = [
+    { id: 1, nombre: "Carne", precio: 1000 },
+    { id: 2, nombre: "Pollo", precio: 1000 },
+    { id: 3, nombre: "Jamon y Queso", precio: 1000 }
+];
 
-    alert(`Hola ${cliente}. Nuestro menú para el día de hoy es:\n\n1. Empanada de carne\n2. Empanada de jamón y queso\n3. Empanada de pollo`);
+// Función para calcular el precio con IVA
+function calcularPrecioConIva(cantidad, precioUnitario) {
+    const iva = 0.21;
+    return cantidad * precioUnitario * (1 + iva);
+}
+
+function bienvenido() {
+    let pedidoArray = []; // Array para almacenar los pedidos
+
+    alert(`Hola ${cliente}. Nuestro menú para el día de hoy es:\n\n1. ${productos[0].nombre}\n2. ${productos[1].nombre}\n3. ${productos[2].nombre}`);
 
     let a = "si";
 
     while (a === "si") {
-        let pedido = prompt("Indícanos que pido de empandas queres compra");
+        let pedidoId = prompt("Indícanos qué tipo de empanada quieres comprar (ingresa el número del producto)\n1. Carne\n2. Pollo\n3. Jamon y Queso");
         let cantidad = prompt("¿Qué cantidad de empanadas?");
         
-        // Acumular el pedido en el objeto
-        if (acumulado[pedido]) {
-            acumulado[pedido] += parseInt(cantidad);
-        } else {
-            acumulado[pedido] = parseInt(cantidad);
-        }
+        // Obtener el producto seleccionado
+        let productoSeleccionado = productos.find(producto => producto.id == pedidoId);
+
+        // Agregar el pedido al array
+        pedidoArray.push({ tipo: productoSeleccionado.nombre, cantidad: parseInt(cantidad) });
 
         a = prompt("¿Te podemos ayudar con algo más? SI o NO").toLowerCase();
     }
 
     // Mostrar el resumen del pedido
     let total = 0;
-    for (let tipo in acumulado) {
-        let precioUnitario = 1000; // Precio por empanada
-        let subtotal = acumulado[tipo] * precioUnitario;
-        total += subtotal;
-        
-    
-        alert(`Pedido acumulado:\n${acumulado[tipo]} empanadas de tipo ${tipo}. Subtotal: $${subtotal}`);
+    for (let pedido of pedidoArray) {
+        let precioUnitario = productos.find(producto => producto.nombre === pedido.tipo).precio;
+
+        // Calcular el precio con IVA
+        let precioConIva = calcularPrecioConIva(pedido.cantidad, precioUnitario);
+        total += precioConIva;
+
+        alert(`Pedido acumulado:\n${pedido.cantidad} empanadas de tipo ${pedido.tipo}. Subtotal con IVA: $${precioConIva.toFixed(2)}`);
     }
-    
-    alert(`Gracias ${cliente} por tu compra. Tu pedido suma total es de $${total}\n\n Por pago en efectivo, tienes un 10% de descuento, quedando en $${total * 0.9}`);
-    }
+
+    alert(`Gracias ${cliente} por tu compra. Tu pedido suma total es de $${total.toFixed(2)}\niva incluido :-)\n\nPor pago en efectivo, tienes un 10% de descuento, quedando en $${(total * 0.9).toFixed(2)}`);
+}
 
 bienvenido();
